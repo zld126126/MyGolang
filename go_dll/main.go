fa65316d0f82fd15ecc631188414d96286496246
dongbao <zld126126@126.com>
2021-10-08 15:08:07 +0800

    update

--- go_dll/main.go
+++ go_dll/main.go
@@ -12,19 +12,29 @@ func IntPtr(n int) uintptr {
 	return uintptr(n)
 }
 
-func GoCallDll1(a, b int) {
+func IsFinishError(err error) bool {
+	if err.Error() == "The operation completed successfully." {
+		return true
+	}
+	return false
+}
+
+func GoCallDll1(a, b int) uintptr {
 	dllFile := syscall.NewLazyDLL(dllFileName)
 	fmt.Println("dll:", dllFile.Name)
 	add := dllFile.NewProc("add")
 	fmt.Println("+++++++NewProc:", add, "+++++++")
 
 	ret, _, err := add.Call(IntPtr(a), IntPtr(b))
-	if err != nil {
+	if err != nil && IsFinishError(err) {
 		fmt.Println(dllFileName+fmt.Sprintf(":%d+%d", a, b)+"运算结果为:", ret)
+	}else{
+		fmt.Println(fmt.Sprintf("%+v",err))
 	}
+	return ret
 }
 
-func GoCallDll2(a, b int) {
+func GoCallDll2(a, b int) uintptr{
 	dllFile, _ := syscall.LoadLibrary(dllFileName)
 	fmt.Println("+++++++syscall.LoadLibrary:", dllFile, "+++++++")
 	defer syscall.FreeLibrary(dllFile)
@@ -36,28 +46,41 @@ func GoCallDll2(a, b int) {
 		IntPtr(a),
 		IntPtr(b),
 		0)
-	if err != nil {
+	if err != nil && IsFinishError(err) {
 		fmt.Println(dllFileName+fmt.Sprintf(":%d+%d", a, b)+"运算结果为:", ret)
+	}else{
+		fmt.Println(fmt.Sprintf("%+v",err))
 	}
+	return ret
 }
 
-func GoCallDll3(a, b int) {
+func GoCallDll3(a, b int) uintptr{
 	DllTestDef := syscall.MustLoadDLL(dllFileName)
 	add := DllTestDef.MustFindProc("add")
 
 	fmt.Println("+++++++MustFindProc：", add, "+++++++")
 	ret, _, err := add.Call(IntPtr(a), IntPtr(b))
-	if err != nil {
-		fmt.Println(dllFileName+fmt.Sprintf(":%d+%d", a, b)+"的运算结果为:", ret)
+	if err != nil && IsFinishError(err) {
+		fmt.Println(dllFileName+fmt.Sprintf(":%d+%d", a, b)+"运算结果为:", ret)
+	}else{
+		fmt.Println(fmt.Sprintf("%+v",err))
 	}
+	return ret
 }
+
+type Common interface{}
 
 // 测试golang 调用 dll方法
 func GoCallDllTest(){
 	// 三种调用方式
-	GoCallDll1(4, 5)
-	GoCallDll2(3, 6)
-	GoCallDll3(2, 7)
+	res1 := GoCallDll1(4, 5)
+	fmt.Println("r1:",(int)(res1))
+
+	res2 := GoCallDll2(3, 6)
+	fmt.Println("r2:",Common(res2))
+
+	res3 := GoCallDll3(2, 7)
+	fmt.Println("r3:",(int)(res3))
 }
 
 // 等待命令
